afb4b8ba346e68c423b329143aa58dbafa6b5a28
Marvin Santiago <marvin.santiago@ymail.com>
2015-08-03 20:32:18 -0500

    Build 0.0.2

--- dist/app.js
+++ dist/app.js
@@ -1,18 +1,28 @@
 AppTemplates = {};
 
-AppTemplates['battle'] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+AppTemplates['battle'] = Handlebars.template({"1":function(depth0,helpers,partials,data,blockParams) {
     var stack1, alias1=this.lambda, alias2=this.escapeExpression;
 
-  return "<div class=\"hero-side\">\n    <img src=\""
-    + alias2(alias1(((stack1 = (depth0 != null ? depth0.hero : depth0)) != null ? stack1.imgURL : stack1), depth0))
+  return "<div class=\"hero-side\">\n    <img src=\"images/"
+    + alias2(alias1(((stack1 = blockParams[0][0]) != null ? stack1.imgURL : stack1), depth0))
     + "\" alt=\""
-    + alias2(alias1(((stack1 = (depth0 != null ? depth0.hero : depth0)) != null ? stack1.name : stack1), depth0))
-    + "\">\n    <p>Health</p>\n</div>\n<div class=\"enemy-side\">\n    <img src=\""
-    + alias2(alias1(((stack1 = (depth0 != null ? depth0.enemy : depth0)) != null ? stack1.imgURL : stack1), depth0))
+    + alias2(alias1(((stack1 = blockParams[0][0]) != null ? stack1.name : stack1), depth0))
+    + "\">\n    <p>Health</p>\n</div>\n";
+},"3":function(depth0,helpers,partials,data,blockParams) {
+    var stack1, alias1=this.lambda, alias2=this.escapeExpression;
+
+  return "<div class=\"enemy-side\">\n    <img src=\"images/"
+    + alias2(alias1(((stack1 = blockParams[0][0]) != null ? stack1.imgURL : stack1), depth0))
     + "\" alt=\""
-    + alias2(alias1(((stack1 = (depth0 != null ? depth0.enemy : depth0)) != null ? stack1.name : stack1), depth0))
-    + "\">\n    <p>Health</p>\n</div>\n<div>\n<button class=\"attack-mode\">Attack</button>\n</div>\n\n\n";
-},"useData":true});
+    + alias2(alias1(((stack1 = blockParams[0][0]) != null ? stack1.name : stack1), depth0))
+    + "\">\n    <p>Health</p>\n</div>\n";
+},"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data,blockParams) {
+    var stack1;
+
+  return ((stack1 = helpers.each.call(depth0,depth0,{"name":"each","hash":{},"fn":this.program(1, data, 1, blockParams),"inverse":this.noop,"data":data,"blockParams":blockParams})) != null ? stack1 : "")
+    + ((stack1 = helpers.each.call(depth0,depth0,{"name":"each","hash":{},"fn":this.program(3, data, 1, blockParams),"inverse":this.noop,"data":data,"blockParams":blockParams})) != null ? stack1 : "")
+    + "<div>\n<button class=\"attack-mode\">Attack</button>\n</div>\n\n\n";
+},"useData":true,"useBlockParams":true});
 AppTemplates['game-over'] = Handlebars.template({"1":function(depth0,helpers,partials,data) {
     return "<h1>You Win!</h1>\n";
 },"3":function(depth0,helpers,partials,data) {
@@ -40,16 +50,10 @@ AppTemplates['start'] = Handlebars.template({"1":function(depth0,helpers,partial
   return "<h1>Protect your city</h1>\n\n<h1>Select Hero</h1>\n"
     + ((stack1 = helpers.each.call(depth0,depth0,{"name":"each","hash":{},"fn":this.program(1, data, 1, blockParams),"inverse":this.noop,"data":data,"blockParams":blockParams})) != null ? stack1 : "");
 },"useData":true,"useBlockParams":true});
-/**
- * @param {[type]}
- * @param {[type]}
- * @param {[type]}
- */
-function Hero(name, health, weapon, imgUrl) {
-  this.name = name;
-  this.health = health;
-  this.weapon = weapon;
-  this.imgUrl = imgUrl;
+  function Character(options) {
+  options = options || {};
+  var hitPoints = options.hitPoints || 100;
+  this.weapons = options.weapons || {};
 
   this.takeDamage = function(damage) { hitPoints -=  damage; };
 
@@ -60,23 +64,54 @@ function Hero(name, health, weapon, imgUrl) {
 
     return 5;
   };
+
+  this.on('attacked', function(amount) {
+    this.takeDamage(amount);
+  });
+
+  this.getHealth = function() {return hitPoints;};
 }
 
-Hero.prototype = {
-  constructor: Hero,
-};
+Character.prototype = _.extend({
+  constructor: Character,
 
-this.name = function() {
-  return this.name;
-};
+  attack: function(hostile, weapon) {
+    // Tell the enemy to that they've been attacked
+    hostile.trigger('attacked', this.getAttackStrength(weapon));
 
-this.health = function() {
-  return this.health;
-};
+    // Directly damaging the enemy
+    // hostile.takeDamage(this.getAttackStrength(weapon));
+  },
+}, Backbone.Events);
 
-this.weapon = function() {
-  return this.weapon;
-};
+char = new Character({hitPoints: 200, weapons: {hammer: 20, sword: 15}});
+enemy = new Character({weapons: {hands: 11, sword: 15}});
+
+// Adds an extra callback to be run ONLY when the enemy instance is attacked
+enemy.on('attacked', function(amount) {
+  if (amount > 10) {
+    console.log('You are a strong foe!');
+  }
+});
+
+char.attack(enemy, 'hammer');
+enemy.attack(char, 'hands');
+
+console.log('character: ', char.getHealth());
+console.log('enemy: ', enemy.getHealth());
+
+/**
+ * @param {[type]}
+ * @param {[type]}
+ * @param {[type]}
+ */
+function Hero(name, health, weapons, imgUrl) {
+  Character.call(this, {hitPoints: health, weapons: weapons});
+  this.name = name;
+  this.imgUrl = imgUrl;
+}
+
+Hero.prototype = _.extend({constructor: Hero}, Character.prototype);
 
 var heroes = [
   new Hero('Arrow', 200, 'arrow', 'arrow.png'),
@@ -84,46 +119,22 @@ var heroes = [
   new Hero('Diggle', 200, 'hands', 'diggle.png'),
 ];
 
-function Enemy(name, health, weapon, imgURL) {
+function Enemy(name, health, weapons, imgURL) {
+  Character.call(this, {hitPoints: health, weapons: weapons});
   this.name = name;
-  this.health = health;
-  this.weapon = weapon;
   this.imgURL = imgURL;
-
-  this.takeDamage = function(damage) { hitPoints -=  damage; };
-
-  this.getAttackStrength = function(weaponName) {
-    if (this.weapons[weaponName]) {
-      return this.weapons[weaponName];
-    }
-
-    return 5;
-  };
 }
 
-Enemy.prototype = {
-  constructor: Enemy,
-};
-
-this.name = function() {
-  return this.name;
-};
-
-this.health = function() {
-  return this.health;
-};
-
-this.weapon = function() {
-  return this.weapon;
-};
+Enemy.prototype = _.extend({constructor: Enemy}, Character.prototype);
 
 var enemies = [
   new Enemy('Slade', 300, 'sword', 'sladewilson.png'),
   new Enemy('Malcolm Merlyn', 200, 'arrow', 'malcolmmerlyn.jpg'),
 ];
 
-function Game() {
-  this.hero = [];
+function Game(hero) {
+  this.hero = hero;
+  this.enemy = _.sample(enemies);
   this.gameOver = true;
   this.playerWins = false;
 }
@@ -131,11 +142,13 @@ function Game() {
 Game.prototype = _.extend({
   constructor: Game,
 
-  attack: function(enemy, ability) {
-    enemy.trigger('attacked', thisgetAttackStrength(ability));
+  attack: function() {
+    this.hero.attack(this.enemy);
+    this.enemy.attack(this.hero);
   },
 
 }, Backbone.Events);
+
 
 $('.game-screen').html(AppTemplates.start(heroes));
 
@@ -143,11 +156,9 @@ $('.game-screen').on('click', '.choose-hero', function() {
   var index = $(this).data('index');
   var game = new Game(heroes[index]);
   $('.game-screen').html(AppTemplates.battle(game));
-});
+  $('.game-screen').on('click', '.attack-mode', function() {
+    game.attack();
+  });
 
-// $('.game-screen').on('click', '.attack-mode', function() {
-//   var index = $(this).data('index');
-//   var game = new Game(heroes[index]);
-//   $('.game-screen').html(AppTemplates.battle(game));
-// });
+});
 //# sourceMappingURL=app.map
